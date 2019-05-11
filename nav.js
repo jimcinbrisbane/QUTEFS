269ec20d7df5741db7073ffb5fecbfa2bbe6d0f8
Jim Chi <48462019+jimcinbrisbane@users.noreply.github.com>
2019-05-11 19:07:17 +1000

    Demo

---
+++ nav.js
@@ -0,0 +1,8 @@
+function navbar() {
+    var x = document.getElementById("topnav");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  } 
