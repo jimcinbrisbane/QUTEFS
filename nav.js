ace6ceae0f51152732d2e1fe65a4a4dc06b059e4
Jim Chi <48462019+jimcinbrisbane@users.noreply.github.com>
2019-07-14 21:31:22 +1000

    Delete nav.js

--- nav.js
+++
@@ -1,8 +0,0 @@
-function navbar() {
-    var x = document.getElementById("topnav");
-    if (x.className === "nav") {
-      x.className += " responsive";
-    } else {
-      x.className = "nav";
-    }
-  } 
