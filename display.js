06b77320d36921765ba02dc83c2acafc2f7d8d49
Jim Chi <48462019+jimcinbrisbane@users.noreply.github.com>
2019-07-14 21:31:49 +1000

    Add files via upload

---
+++ display.js
@@ -0,0 +1,15 @@
+function start() {
+    var display_home = document.getElementById("home");
+    var hide_about = document.getElementById("about");
+    display_home.style.display = "block";
+    hide_about.style.display = "none";
+
+}
+
+function about() {
+    var display_about = document.getElementById("about");
+    var hide_home = document.getElementById("home");
+    display_about.style.display = "block";
+    hide_home.style.display = "none";
+
+}
---
+++ nav.js
@@ -0,0 +1,9 @@
+//mobile
+function navbar() {
+    var x = document.getElementById("topnav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
