4434eb32b60c07ec049e8298aeb684945aade183
Jack <jack.ber.private@gmail.com>
2019-07-26 00:13:46 +1000

    better page animations

--- display.js
+++ display.js
@@ -4,14 +4,16 @@ let switching = false;
 $(()=>{
     $('#topnav a').click(ev => {
         let newPage = $(ev.target).data('to');
-        if(newPage){
-            if(!switching){
-                switching = true;
-                $(`#${current}`).fadeOut('fast', ()=>{
-                    current = newPage;
-                    $(`#${newPage}`).fadeIn('fast', ()=>{ switching = false });
+        if(newPage && !switching && newPage !== current){
+            switching = true;
+            $(`#${current}`).animate({ opacity: 0, marginTop: '15px' }, 300, ()=>{
+                $(`#${current}`).hide();
+
+                current = newPage;
+                $(`#${newPage}`).css({ opacity: 0, marginTop: '15px' }).show().animate({ opacity: 1, marginTop: '0px' }, 300, ()=>{ 
+                    switching = false;
                 });
-            }
+            });
         }
     })
 });
