bf0eee9ecbf95ef3dbd847edb967ca1cace507f0
Jack <jack.ber.private@gmail.com>
2019-07-25 23:45:45 +1000

    page transitions

--- display.js
+++ display.js
@@ -1,36 +1,17 @@
-function start() {
-    document.getElementById("public").style.display = "none";
-    document.getElementById("home").style.display = "block";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("partners").style.display = "none";
+let current = 'home';
+let switching = false;
 
+function switchTo(newPage){
+    console.log(`Transitioning from ${current} to ${newPage}`);
+
+    if(!switching){
+        switching = true;
+        $(`#${current}`).fadeOut('fast', ()=>{
+            current = newPage;
+            $(`#${newPage}`).fadeIn('fast', ()=>{ switching = false });
+        });
+    }
 }
-
-function about() {
-    document.getElementById("public").style.display = "none";
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "block";
-    document.getElementById("partners").style.display = "none";
-
-}
-
-function public() {
-    document.getElementById("public").style.display = "block";
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("partners").style.display = "none";
-
-}
-
-function partner() {
-    document.getElementById("partners").style.display = "block";
-    document.getElementById("public").style.display = "none";
-    document.getElementById("home").style.display = "none";
-    document.getElementById("about").style.display = "none";
-
-
-}
-
 
 /*
     document.getElementById("partners").style.display = "block";
--- nav.js
+++ nav.js
@@ -7,3 +7,5 @@ function navbar() {
         x.className = "nav";
     }
 }
+
+console.log($)
